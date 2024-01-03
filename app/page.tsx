992cd86328666714013f2867373f38e1716a3210
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-01-03 00:19:20 -0600

    implement base tailwind cofig and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import "../styles/theme.css";
-
 export default function Home() {
   return (
     <div>
       <main>
-        <h1 className="bold">ChatGPT helped me build this</h1>
+        <h1 className="h1-bold">Next.js 13 here we go</h1>
+        <h2 className="h2-bold">Next.js 13 here we go</h2>
       </main>
     </div>
-  );
+  )
 }
