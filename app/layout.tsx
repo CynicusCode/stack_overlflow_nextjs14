244c2c261ae2c9cbea8daac80346772325777bbc
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-01-05 01:07:36 -0600

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,28 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import React from 'react'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'Devflow',
+  description:
+    'Devflow is a platform for developers to share their projects and get feedback from other developers.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
 
 export default function RootLayout({
   children
@@ -8,10 +30,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          fromButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
+      <div className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        {children}
+      </div>
     </ClerkProvider>
   )
 }
